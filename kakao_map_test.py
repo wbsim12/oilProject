7544f129240ac523d974fe93bdc5800f14afebca
wbsim12 <simjaewoo12@naver.com>
2024-04-30 09:15:45 +0900

    0430_am_commit_sim

--- kakao_map_test.py
+++ kakao_map_test.py
@@ -1,6 +1,6 @@
 import os, sys
 import traceback
-import calc
+
 import sys
 # pyQt 라이브러리 임포트
 from PyQt5.QtCore import QUrl
@@ -9,7 +9,7 @@ from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 root = os.path.dirname(os.path.abspath(__file__))
 # ui만든 파일의 경로
-MainUI = uic.loadUiType(os.path.join(root, 'web_test.ui'))[0]
+MainUI = uic.loadUiType(os.path.join(root, 'testMain.ui'))[0]
 
 class MainDialog(QMainWindow, MainUI):
 
@@ -19,13 +19,15 @@ class MainDialog(QMainWindow, MainUI):
         super().__init__()
         self.setupUi(self)
         self.setWindowTitle("다이나믹 맵 테스트")
+        self.web_view.load(QUrl("http://localhost:8000/kao_index.html"))
+        # with open('http://localhost:8000/', 'r', encoding='utf-8') as f:
+        #     html_content = f.read()
+        # self.web_view.setHtml(html_content)
 
-        self.load_kakao_map()
-
-    def load_kakao_map(self):
-        # Kakao Map API를 사용한 HTML 페이지 URL
-        url = QUrl.fromLocalFile("http://localhost:8080")  # HTML 파일 경로 입력
-        self.web_view.load(url)
+    # def load_kakao_map(self):
+    #     # Kakao Map API를 사용한 HTML 페이지 URL
+    #     url = QUrl.fromLocalFile("http://localhost:8080")  # HTML 파일 경로 입력
+    #     self.web_view.load(url)
 
 
 if __name__ == '__main__':
